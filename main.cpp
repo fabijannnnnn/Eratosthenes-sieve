bd482b7abdfab990c975156facd6eb27bcb97104
fabijannn <fabianuhrin1@gmail.com>
2024-11-18 20:39:49 +0100

    added functions for computation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
+#include <cstdio>
 #include <iostream>
 
 void writePrimes(bool* arr, int size)
 {
     std::cout << "Prime numbers: " <<std::endl;
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i])
+            std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
 
+void removeMultipliers(bool* arr, int multiplier, int size)
+{
+    for(int i = multiplier * 2; i < size; i += multiplier)
+    {
+        arr[i] = false;
+    }
+}
+
+void calculatePrimes(bool* arr, int size)
+{
+    for(int i = 0; i <= sqrt(size); i++)
+    {
+        if(arr[i])
+            removeMultipliers(arr, i, size);
+    }
 }
 
 void initArr(bool* arr, int size)
@@ -25,7 +48,11 @@ int main()
     bool* arr = new bool[number + 1];
     initArr(arr, number + 1);
 
+    calculatePrimes(arr, number + 1);
+
     writePrimes(arr, number + 1);
+
+    delete []arr;
 
     return 0;
 }
